a4307d0efa15d724623708d3ad07532242679074
silas <silasgebhart12@gmail.com>
2024-01-22 18:09:32 -0600

    Adding in more functions.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function makeHappy() {
+function reasonForDallasNotMakingSuperBowl() {
   document.getElementById("Dak").src = "dallas-cowboys-loss-49ers.png";
+  document.getElementById("Star").innerHTML = "Dak Prescott cannot throw!";
+}
+function whyDoesDallasNotMakeTheSuperBowl() {
+  document.getElementById("Dak").src = "dal.png";
+  document.getElementById("Star").innerHTML = "Dak Prescott cannot throw!";
 }
